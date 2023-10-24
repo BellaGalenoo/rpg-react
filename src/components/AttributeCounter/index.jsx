c1ea47250e261676c0627b16010691946ecce737
Jana <jana@Galenos-Mac-mini.local>
2023-10-23 23:38:10 -0300

    fix import indentention

--- src/components/AttributeCounter/index.jsx
+++ src/components/AttributeCounter/index.jsx
@@ -1,14 +1,14 @@
 export default function AttributeCounter() {
-    return (
-        <div >
-            <button>-</button>
-            <select name="numero" id="numero" className="bg-transparent">
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <button>+</button>
-        </div>
-    )
+  return (
+    <div>
+      <button>-</button>
+      <select name="numero" id="numero" className="bg-transparent">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <button>+</button>
+    </div>
+  )
 }
